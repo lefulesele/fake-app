2c439f72297d6973d6ce6f139fa6f746f14cefe9
matseliso <makobotimolefi@gmail.com>
2025-10-28 10:53:55 +0200

    fix: add react-bootstrap dependency for build

--- build/static/js/488.f03d547e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkluct_faculty_system=self.webpackChunkluct_faculty_system||[]).push([[488],{488:(e,s,t)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.f03d547e.chunk.js.map
